1e27b9780c98ccbee76b24c30a4e4fa273e9af2a
AndreeaAVic <andreeahidigan6@gmail.com>
2021-06-02 14:44:57 +0300

    deleting unnecessary information

--- src/components/BackgroundPattern/BackgroundPattern.jsx
+++ src/components/BackgroundPattern/BackgroundPattern.jsx
@@ -5,6 +5,5 @@ import "./BackgroundPattern.css";
 export function BackgroundPattern() {
     return (
         <img src={backgroundPattern} alt="pattern" id="patternImg" />
-        //  <a href='https://pngtree.com/free-backgrounds'>free background photos from pngtree.com</a>
-    )
+    );
 }
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -60,7 +60,5 @@ export function About() {
                 </Fade>
             </div>
         </div>
-        // <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
-        // <div>Icons made by <a href="" title="DinosoftLabs">DinosoftLabs</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
     );
 }
